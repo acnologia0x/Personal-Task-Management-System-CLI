7533813f7d7bce82e8d820d5293fc78a02534da0
acnologia0x <shirou29294@gmail.com>
2026-07-07 12:58:07 +0700

    I made a logic error in my loop structure  by adding some unnecessary loop and if statements

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 to_Do_List = []
 
 def add_Task(recieved_List):
-    print("It worked!")
-
+#  I did NOT need to loop through the existing list to add a new task. I simply need to capture the input and append it.
+    for tasks in recieved_List:
+        if len(tasks) == 0:
+            print("=================")
+            print("No tasks.")
+            print("=================")
+        else:
+            #By putting input() inside of an else statement I am basically saying only ask to add task if the task is not empty lol it defeats the whole point of adding tasks to an empty list.
+            task_Name = input("What to do: ")
+            priority = input("Task Priority(1.High, 2.Medium, 3.Low): ")
+            new_Task = {
+                'task' : task_Name,
+                'pri' : priority
+            }   
+            recieved_List.append(new_Task)
 def menu():
     print("1. Add a Task.")
     print("2. View All Tasks.")
