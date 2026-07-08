d88acba8ee83cf362e5c0b3aa3a91508fdc98f7d
acnologia0x <shirou29294@gmail.com>
2026-07-08 12:20:43 +0700

    test2

--- main.py
+++ main.py
@@ -23,8 +23,5 @@ def main():
             add_Task(to_Do_List)
         elif user_input == '5':
             break
-def test(){
-    print("TEST")
-}
 # <================================================MAIN PROGRAM STARTS HERE================================================>
-test()
+main()
---
+++ test folder/main.py
@@ -0,0 +1,2 @@
+print("TEST ECHO")
+print("WHATTTT")
